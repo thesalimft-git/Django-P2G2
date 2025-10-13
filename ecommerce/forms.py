6923b848f4e94c21694e5b84a3bc9e5f2b0e2881
sali mft <thesalimft@gmail.com>
2025-10-13 18:56:36 +0330

    add class to forms

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 
-class ProductForm(forms.Form):
-    name = forms.CharField(max_length=250,required=False)
-    price = forms.DecimalField(required=False)
 
-    
+class ProductForm(forms.Form):
+    name = forms.CharField(
+        max_length=250,
+        required=False,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    price = forms.DecimalField(
+        required=False,
+        widget=forms.NumberInput(attrs={'class': 'form-control'})
+    )
     
