a240f256e5b48039b802533e17d969b058364168
sali mft <thesalimft@gmail.com>
2025-10-25 17:03:28 +0330

    add detail view for products

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('store/', views.StorePageView.as_view(), name='store_page'),
+    path('products/', views.StorePageView.as_view(), name='store_page'),
+    path('products/<int:id>', views.ProductDetailPageView.as_view(), name='product_detail_page'),
+    
+    
     path('login/', views.LoginPageView.as_view(), name='login_page'),
     path('logout/', views.LogoutPage.as_view(), name='logout_page'),
     path('new-product/', views.NewProductView.as_view(), name='new_product'),
